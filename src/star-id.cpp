ec00661bb0e8cbd6b6850151eb49c2ffdde92d78
Sathvik <sathvikchinta@gmail.com>
2021-01-13 17:59:15 -0800

    some math but it dont work rn

--- src/star-id.cpp
+++ src/star-id.cpp
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 
+#include <math.h>
 #include "star-id.hpp"
+#include "databases.hpp"
 
 namespace lost {
 
@@ -20,18 +22,31 @@ StarIdentifiers DummyStarIdAlgorithm::Go(
 
 StarIdentifiers GeometricVotingStarIdAlgorithm::Go(
     const unsigned char *database, const Stars &stars, const Camera &camera) const {
-    //convert x and y coordinates to degree differences 
-    //give a greter range for min-max Query for bigger radius (GreatCircleDistance)
-    //US voting system 
+    //make star datastructure that will keep track of the votes?
+    for (const Star &i : stars) {
+        //convert x and y coordinates to degree differences 
+        //ascension or declination = arctan(xpos/(xRes/2/tan(FOV/2)))
+        float declination = atan(i.x/(camera.xResolution/2/tan(camera.xFov/2)));
+        float rascension = atan(i.y/(camera.xResolution/2/tan(camera.xFov/2)));
+        for (const Star &j: stars) {
+            if (&i != &j) {
+                float secondStarDeclination = atan(j.x/(camera.xResolution/2/tan(camera.xFov/2)));
+                float secondStarRascension = atan(i.y/(camera.xResolution/2/tan(camera.xFov/2)));
+                float GCD = 2.0*asin(sqrt(pow(sin(abs(declination-secondStarDeclination)/2.0), 2.0)
+                         + cos(declination)*cos(secondStarDeclination)*pow(sin(abs(rascension-secondStarRascension)/2.0), 2.0)));
+                //give a greter range for min-max Query for bigger radius (GreatCircleDistance)
+                float lowerBoundRange = GCD + i.radiusX/2 + j.radiusX/2;
+                float upperBoundRange = lowerBoundRange + i.radiusX/2 + j.radiusX/2;
+                //if database is a KVectorDatabase
+                int *numReturnedPairs; 
+                int lowerBoundSearch = database.FindPossiblestarPairsApprox(lowerBoundRange, upperBoundRange, numReturnedPairs);
+                //loop from lowerBoundSearch till numReturnedPairs, add one vote to each star in the pairs in the datastruct
+                //US voting system 
+            }
+        }
+    }
     //optimizations? N^2
     //testing, add false stars and see if the accuracy is still good (maybe just 1 or 2 false stars)
-}
-
-StarIdentifiers GeometricVotingStarIdAlgorithm::Go(
-    const unsigned char *database, const Stars &stars, const Camera &camera) const {
-
-    // TODO
-    ;
 }
 
 StarIdentifiers PyramidStarIdAlgorithm::Go(
