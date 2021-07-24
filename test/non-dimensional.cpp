f59d2347a543154064739740b05e0de7d3e18f13
Armand Mousavi <amousavi@cs.washington.edu>
2021-07-24 07:01:11 -0400

    corrected test

--- test/non-dimensional.cpp
+++ test/non-dimensional.cpp
@@ -14,26 +14,25 @@ using namespace lost;
 
 // three stars that should be easy to identify based on their distances to each other
 // lets see how it performs when the angles are more important
-Catalog tripleCatalog = {
+const Catalog catalog = {
     CatalogStar(DegToRad(0), DegToRad(0), 1.0, 42),
-    CatalogStar(DegToRad(90), DegToRad(0), 0, 43),
-    CatalogStar(DegToRad(90), DegToRad(90), 1.0, 44),
+    CatalogStar(DegToRad(90), DegToRad(90), 1.0, 43),
+    CatalogStar(DegToRad(90), DegToRad(-90), 1.0, 44),
 };
 
 TEST_CASE("inner angle test simple", "[gv] [fast]") {
-    const Catalog &catalog = GENERATE((const Catalog &)tripleCatalog);
     float min;
     float mid;
     float max;
     int mindex;
     int middex;
     int maxdex;
-    innerAngles(catalog, min, mid, max, mindex, middex, maxdex, 42, 43, 44);
-    std::cout << "mindex: " << mindex << std::endl;
-    std::cout << "middex: " << middex << std::endl;
-    std::cout << "maxdex: " << maxdex << std::endl;
-    CHECK(min == DegToRad(45));
-    CHECK(max == DegToRad(90));
+    innerAngles(catalog, min, mid, max, mindex, middex, maxdex, 0, 1, 2);
+    std::cout << "mindex: " << mindex << " val: " << min << std::endl;
+    std::cout << "middex: " << middex << " val: " << mid << std::endl;
+    std::cout << "maxdex: " << maxdex << " val: " << max << std::endl;
+    CHECK(min == Approx(DegToRad(45)));
+    CHECK(max == Approx(DegToRad(90)));
     //REQUIRE(comparison.numTotal == catalog.size());
     //CHECK(comparison.numCorrect == catalog.size());
 }
@@ -53,9 +52,9 @@ TEST_CASE("focal plane angle test simple", "[gv] [fast]") {
     int middex;
     int maxdex;
     focalPlaneAngles(stars, min, mid, max, mindex, middex, maxdex, 0, 1, 2);
-    CHECK(max == DegToRad(90));
-    CHECK(mid == DegToRad(45));
-    CHECK(min == DegToRad(45));
+    CHECK(max == Approx(DegToRad(90)));
+    CHECK(mid == Approx(DegToRad(45)));
+    CHECK(min == Approx(DegToRad(45)));
     CHECK(mindex > 0);
     CHECK(middex > 0);
     CHECK(maxdex == 0);
