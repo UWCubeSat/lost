ac6366639f23395bd36ebdb2b062c71eddf0f06f
Karen Haining <khaining@cs.washington.edu>
2022-07-08 16:38:04 -0700

    Implement QUEST attitude estimation algorithm

--- src/attitude-estimators.cpp
+++ src/attitude-estimators.cpp
@@ -4,13 +4,14 @@
 
 namespace lost {
 
-Attitude DavenportQAlgorithm::Go(const Camera &camera,
+#define EPSILON 0.0001       // threshold for 0 for Newton-Raphson method
+
+DqmQuestHelperMatrices DqmQuestHelperMatricesConstructor(const Camera &camera,
                                  const Stars &stars,
                                  const Catalog &catalog,
                                  const StarIdentifiers &starIdentifiers) {
 
-    //create a vector that'll hold {bi} (Stars in our frame)
-    //create a vector that'll hold {ri} (Stars in catalog frame)
+    // attitude profile matrix
     Eigen::Matrix3f B;
     B.setZero();
     for (const StarIdentifier &s : starIdentifiers) {
@@ -24,9 +25,10 @@ Attitude DavenportQAlgorithm::Go(const Camera &camera,
         ri << rStar.spatial.x, rStar.spatial.y, rStar.spatial.z;
 
         //Weight = 1 (can be changed later, in which case we want to make a vector to hold all weights {ai})
-        //Calculate matrix B = sum({ai}{bi}{ri}T)
+        // Calculate matrix B = sum({ai}{bi}{ri}T)
         B += ri * bi.transpose() * s.weight;
     }
+
     // S = B + Transpose(B)
     Eigen::Matrix3f S = B + B.transpose();
     //sigma = B[0][0] + B[1][1] + B[2][2]
@@ -36,12 +38,31 @@ Attitude DavenportQAlgorithm::Go(const Camera &camera,
     Z << B(1,2) - B(2,1),
         B(2,0) - B(0,2),
         B(0,1) - B(1,0);
-    //K =  [[[sigma], [Z[0]], [Z[1]], [Z[2]]], [[Z[0]], [S[0][0] - sigma], [S[0][1]], [S[0][2]]], [[Z[1]], [S[1][0]], [S[1][1] - sigma], [S[1][2]]], [[Z[2]], [S[2][0]], [S[2][1]], [S[2][2] - sigma]]]
+
+    // only used for Davenport matrix
+    // K =  [[[sigma], [Z[0]], [Z[1]], [Z[2]]], [[Z[0]], [S[0][0] - sigma], [S[0][1]], [S[0][2]]], [[Z[1]], [S[1][0]], [S[1][1] - sigma], [S[1][2]]], [[Z[2]], [S[2][0]], [S[2][1]], [S[2][2] - sigma]]]
     Eigen::Matrix4f K;
     K << sigma, Z(0), Z(1), Z(2),
         Z(0), S(0,0) - sigma, S(0,1), S(0,2),
         Z(1), S(1,0), S(1,1) - sigma, S(1,2),
         Z(2), S(2,0), S(2,1), S(2,2) - sigma;
+
+    DqmQuestHelperMatrices m;
+    m.K = K;
+    m.S = S;
+    m.Z = Z;
+    m.sigma = sigma;
+    return m;
+}
+
+Attitude DavenportQAlgorithm::Go(const Camera &camera,
+                                 const Stars &stars,
+                                 const Catalog &catalog,
+                                 const StarIdentifiers &starIdentifiers) {
+
+    // Create matrix
+    Eigen::Matrix4f K = DqmQuestHelperMatricesConstructor(camera, stars, catalog, starIdentifiers).K;
+
     //Find eigenvalues of K, store the largest one as lambda
     //find the maximum index
     Eigen::EigenSolver<Eigen::Matrix4f> solver(K);
@@ -55,6 +76,7 @@ Attitude DavenportQAlgorithm::Go(const Camera &camera,
             maxEigenvalue = values(i).real();
         }
     }
+
     //The return quaternion components = eigenvector assocaited with lambda
     Eigen::Vector4cf maxEigenvector = vectors.col(maxIndex);
     // IMPORTANT: The matrix K is symmetric -- clearly first row and first column are equal.
@@ -101,6 +123,70 @@ Attitude TriadAlgorithm::Go(const Camera &camera,
     // have the relationship photoFrame = attitude*catalogFrame, and since catalogFrame is an
     // orthogonal matrix, we have attitude = photoFrame*transpase(catalogFrame)
     return Attitude(photoFrame*catalogFrame.Transpose());
+}
+
+/**
+ * Characteristic polynomial of the quest K-matrix
+ * @see equation 19b of https://arc.aiaa.org/doi/pdf/10.2514/1.62549
+ */
+float QuestCharPoly(float x, float a, float b, float c, float d, float s) {return (pow(x,2)-a) * (pow(x,2)-b) - (c*x) + (c*s) - d;}
+
+/**
+ * Derivitive of the characteristic polynomial of the quest K-matrix
+ */
+float QuestCharPolyPrime(float x, float a, float b, float c) {return 4*pow(x,3) - 2*(a+b)*x - c;}
+
+/**
+ * Approximates roots of a real function using the Newton-Raphson algorithm 
+ * @see https://www.geeksforgeeks.org/program-for-newton-raphson-method/
+ */
+float QuestEigenvalueEstimator(float guess, float a, float b, float c, float d, float s) {
+    float height;
+    do {
+        height = QuestCharPoly(guess, a, b, c, d, s) / QuestCharPolyPrime(guess, a, b, c);
+        guess -= height;
+    } while (abs(height) >= EPSILON);
+
+    return guess;
+}
+
+Attitude QuestAlgorithm::Go(const Camera &camera,
+                            const Stars &stars,
+                            const Catalog &catalog,
+                            const StarIdentifiers &starIdentifiers) {
+
+    // create matrix
+    DqmQuestHelperMatrices m = DqmQuestHelperMatricesConstructor(camera, stars, catalog, starIdentifiers);
+
+    // calculate coefficients for characteristic polynomial
+    float delta = m.S.determinant();
+    float kappa = (delta * m.S.inverse()).trace();
+    float a = pow(m.sigma,2) - kappa;
+    float b = pow(m.sigma,2) + (m.Z.transpose() * m.Z);
+    float c = delta + (m.Z.transpose() * m.S * m.Z);
+    float d = m.Z.transpose() * (m.S * m.S) * m.Z;
+
+    // sum up the weights for initial guess of eigenvalue
+    float guess = 0;
+    for (const StarIdentifier &s : starIdentifiers) {
+        guess += s.weight;
+    }
+
+    // Newton-Raphson method for estimating the largest eigenvalue
+    float eig = QuestEigenvalueEstimator(guess, a, b, c, d, m.sigma);
+
+    // solve for the optimal quaternion: from https://ahrs.readthedocs.io/en/latest/filters/quest.html
+    float alpha = pow(eig,2) - pow(m.sigma, 2) + kappa;
+    float beta = eig - m.sigma;
+    float gamma = (eig + m.sigma) * alpha - delta;
+
+    Eigen::Vector3f X = ((alpha * Eigen::Matrix3f::Identity()) + (beta * m.S) + (m.S * m.S)) * m.Z;
+
+    Eigen::Vector4f optQuat;
+    optQuat << gamma, X[0], X[1], X[2];
+    optQuat /= optQuat.norm();
+
+    return Attitude(Quaternion(optQuat[0], optQuat[1], optQuat[2], optQuat[3]));
 }
 
 }
--- src/attitude-estimators.hpp
+++ src/attitude-estimators.hpp
@@ -1,11 +1,26 @@
 #ifndef ATTITUDE_ESTIMATORS_H
 #define ATTITUDE_ESTIMATORS_H
 
+#include <eigen3/Eigen/Eigenvalues>
+
 #include "attitude-utils.hpp"
 #include "camera.hpp"
 #include "star-id.hpp"
 
 namespace lost {
+
+/// Necessary matrix information for both the dqm and quest algos
+struct DqmQuestHelperMatrices {
+    Eigen::Matrix4f K;      /// Davenport matrix
+    Eigen::Matrix3f S;      /// B + B^T (where B is the attitude profile matrix)
+    Eigen::Vector3f Z;      /// [B23 - B32, B31 - B13, B12 - B21]^T]
+    float sigma;            /// tr(B)
+};
+
+/**
+ * Returns necessary matrix information for calculations in both the dqm and quest algorithms
+ */
+DqmQuestHelperMatrices DqmQuestHelperMatricesConstructor(const Camera &camera, const Stars &stars, const Catalog &catalog, const StarIdentifiers &starIdentifiers);
 
 /**
  * An attitude estimation algorithm estimates the orientation of the camera based on identified stars.
@@ -37,6 +52,15 @@ public:
  * TRIAD is prone to error if either of the selected stars are not good. Even if both selected stars are decent, it's better to use an attitude estimator that takes into account all the stars to better cancel out centroiding noise.
  */
 class TriadAlgorithm : public AttitudeEstimationAlgorithm {
+public:
+    Attitude Go(const Camera &, const Stars &, const Catalog &, const StarIdentifiers &);
+};
+
+/**
+ * A faster and just as accurate attitude estimator as the Davenport Q algorithm.
+ * Historically important as the de facto standard attitude estimation algorithm.
+ */
+class QuestAlgorithm : public AttitudeEstimationAlgorithm {
 public:
     Attitude Go(const Camera &, const Stars &, const Catalog &, const StarIdentifiers &);
 };
