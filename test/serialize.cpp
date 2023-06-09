f5e31bc17baa20f7208d908b85e33dab0a39e575
Mark Polyakov <mark@markasoftware.com>
2023-06-09 16:20:56 -0700

    Add couple more tests.

--- test/serialize.cpp
+++ test/serialize.cpp
@@ -62,3 +62,38 @@ TEST_CASE("Endian-swapped floats only", "[fast] [serialize]") {
     float redeserializedValFloat = DeserializePrimitive<float>(&des2);
     CHECK(valFloat == redeserializedValFloat);
 }
+
+TEST_CASE("Padding", "[fast] [serialize]") {
+    int8_t val8 = 23;
+    int32_t val32 = 1234567;
+    SerializeContext ser;
+    SerializePrimitive<int8_t>(&ser, val8);
+    SerializePrimitive<int32_t>(&ser, val32);
+    CHECK(ser.buffer.size() == 8);
+    CHECK(ser.buffer[0] == 23);
+    CHECK(ser.buffer[1] == 0);
+    CHECK(ser.buffer[2] == 0);
+    CHECK(ser.buffer[3] == 0);
+
+    DeserializeContext des(ser.buffer.data()+4);
+    int32_t deserializedVal32 = DeserializePrimitive<int32_t>(&des);
+    CHECK(val32 == deserializedVal32);
+}
+
+TEST_CASE("Array", "[fast] [serialize]") {
+    SerializeContext ser;
+    // serialize a single byte first, to ensure that array adds padding.
+    SerializePrimitive<int8_t>(&ser, 42);
+    for (int16_t i = 0; i < 16; i++) {
+        SerializePrimitive<int16_t>(&ser, i);
+    }
+
+    DeserializeContext des(ser.buffer.data());
+    int8_t firstByte = DeserializePrimitive<int8_t>(&des);
+    CHECK(firstByte == 42);
+    const int16_t *arr = DeserializeArray<int16_t>(&des, 8);
+    CHECK(arr[0] == 0);
+    CHECK(arr[1] == 1);
+    int8_t ninthByte = DeserializePrimitive<int16_t>(&des);
+    CHECK(ninthByte == 8);
+}
